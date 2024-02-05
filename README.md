625a2ef2107f909bb51f244458238341f67486fc
Jose Campusano <josecampusano0427@gmail.com>
2024-02-04 22:17:12 -0400

    Ej-00 Completado

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mis soluciones a los retos de Programacion de Mouredev
+
+Con Java y Typescript
+
---
+++ src/typescript/ej00.ts
@@ -0,0 +1,25 @@
+// Comentario con la url Oficial = https://www.typescriptlang.org/
+
+// Este es un comentario de una linea
+
+/**
+ * Este Es un comentario
+ * De Varias lineas
+ */
+
+// ** Variable
+let variable;
+
+// ** Constante
+const constante = 'constante';
+
+// ** Datos primitivos
+let miString: string = 'Hola Typescript!';
+let miNumero: number = 15;
+let miBooleano: boolean = true;
+let miSymbol: symbol = Symbol('5');
+let miUndefined: undefined = undefined;
+let miNull: null = null;
+
+// ** Imprimir por consola
+console.log(miString)
